5cb5018b55b03d76a6e84c3b28a781e605f49490
Krishna <krishna61223@gmail.com>
2024-09-07 00:57:26 +0530

    config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
